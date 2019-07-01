dd0bd2c1020d6aac88646a1a364862ab7187d876
Lukasz 'iniside' Baran <iniside@live.com>
2019-07-01 22:16:33 +0200

    comment ouut repository code to fix compilation.

--- Spark.Identity/SparkResourceOwnerPasswordValidator.cs
+++ Spark.Identity/SparkResourceOwnerPasswordValidator.cs
@@ -9,20 +9,20 @@ namespace Spark.Identity
 {
     public class SparkResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
     {
-        private readonly IUserRepository _userRepository;
-
-        public SparkResourceOwnerPasswordValidator(IUserRepository userRepository)
-        {
-            _userRepository = userRepository;
-        }
+        //private readonly IUserRepository _userRepository;
+        //
+        //public SparkResourceOwnerPasswordValidator(IUserRepository userRepository)
+        //{
+        //    _userRepository = userRepository;
+        //}
 
         public Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
         {
-            if (_userRepository.ValidateCredentials(context.UserName, context.Password))
-            {
-                var user = _userRepository.FindByUsername(context.UserName);
-                context.Result = new GrantValidationResult(user.SubjectId, OidcConstants.AuthenticationMethods.Password);
-            }
+            //if (_userRepository.ValidateCredentials(context.UserName, context.Password))
+            //{
+            //    var user = _userRepository.FindByUsername(context.UserName);
+            //    context.Result = new GrantValidationResult(user.SubjectId, OidcConstants.AuthenticationMethods.Password);
+            //}
 
             return Task.FromResult(0);
         }
