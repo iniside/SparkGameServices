f21ba38d7e3bf70ff3d34adba440a32ed6f91b62
Lukasz 'iniside' Baran <iniside@live.com>
2019-07-04 19:49:45 +0200

    Handle responding to sample message.

--- Spark.TestService/Program.cs
+++ Spark.TestService/Program.cs
@@ -1,12 +1,78 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using RawRabbit;
+using RawRabbit.Configuration;
+using RawRabbit.Enrichers.GlobalExecutionId;
+using RawRabbit.Enrichers.MessageContext;
+using RawRabbit.Enrichers.MessageContext.Context;
+using RawRabbit.Instantiation;
+using Spark.Events;
 
+namespace Spark.Events
+{
+    public class MessageWrapper
+    {
+        public string MessageType { get; set; }
+
+        public string MessageBody { get; set; }
+    }
+
+    public class TestMessage
+    {
+        public string UserId { get; set; }
+        public string SomeMessage { get; set; }
+    }
+
+    public class TestMessageResponse
+    {
+        public string UserId { get; set; }
+        public string SomeMessage { get; set; }
+    }
+}
 namespace Spark.TestService
 {
     class Program
     {
-        static void Main(string[] args)
+        private static IBusClient _client;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        public static async Task RunAsync()
+        {
+            //Log.Logger = new LoggerConfiguration()
+            //    .WriteTo.LiterateConsole()
+            //    .CreateLogger();
+
+            _client = RawRabbitFactory.CreateSingleton(new RawRabbitOptions
+            {
+                ClientConfiguration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("rawrabbit.json")
+                    .Build()
+                    .Get<RawRabbitConfiguration>()
+            });
+
+            //Type t = Type.GetType("AskEvent");
+            //var tt = typeof(AskEvent);
+            //
+            //object o = new AskEvent { Question = "Ask Question" };
+            //
+            //await _client.PublishAsync(o);
+            await _client.SubscribeAsync<TestMessage>(ServerValuesAsync);
+            //await _client.RespondAsync<ValueRequest, ValueResponse>(request => SendValuesThoughRpcAsync(request));
+        }
+        private static  async Task ServerValuesAsync(TestMessage message)
+        {
+            TestMessageResponse respond = new TestMessageResponse();
+            respond.UserId = message.UserId;
+            respond.SomeMessage = "responded to you!";
+
+            await _client.PublishAsync(respond);
         }
     }
 }
