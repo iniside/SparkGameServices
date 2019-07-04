02731545c824419005ade38c325d89d262768c7f
Lukasz 'iniside' Baran <iniside@live.com>
2019-07-04 17:43:54 +0200

    Add service which will respond to test messages.

---
+++ Spark.TestService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Spark.TestService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
