291c93beef55a3fd738f145dfa0ec7a7feae4a21
Lukasz 'iniside' Baran <iniside@live.com>
2019-07-04 17:42:47 +0200

    Add login to sample client.

--- Spark.SignalRClient/Program.cs
+++ Spark.SignalRClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
 
@@ -10,21 +11,34 @@ namespace Spark.SignalRClient
     {
         public String token { get; set; }
     }
+    public class LoginWithProviderModel
+    {
+        public string Provider { get; set; }
+        public string ExternalId { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             HttpClient client = new HttpClient();
-
+            
             List<KeyValuePair<string, string>> pairs = new List<KeyValuePair<string, string>>();
-
+            LoginWithProviderModel m = new LoginWithProviderModel
+            {
+                Provider = "Provider",
+                ExternalId = "87as5drtifuykvjha"
+            };
+            
+            string ms =  System.Text.Json.Serialization.JsonSerializer.ToString(m);
+            
             pairs.Add(new KeyValuePair<string, string>("ProviderName", "Microsoft"));
             pairs.Add(new KeyValuePair<string, string>("ProviderKey", "c9cc8a70ad19c2bc"));
-
-            HttpContent content = new FormUrlEncodedContent(pairs);
-
-            var r = client.PostAsync("https://localhost:8001/api/values/login", new StringContent("")).Result;//, content).Result;
-
+            
+            HttpContent content = new StringContent(ms);
+            content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            
+            var r = client.PostAsync("https://localhost:8001/api/values/LoginWithExternalProvider", content).Result;//, content).Result;
+            
             string c = r.Content.ReadAsStringAsync().Result;
             //var dd = System.Text.Json.Serialization.JsonSerializer.Parse<TokenClass>(c);
 
